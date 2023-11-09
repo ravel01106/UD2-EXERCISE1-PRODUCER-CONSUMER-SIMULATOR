7fe76e79654ef7e31b7fc5299252063489399891
ravel01106 <ravello2002@gmail.com>
2023-11-09 22:14:56 +0000

    feat(Farmer): create Farmer thread class

---
+++ src/net/salesianos/exercise/farmer/Farmer.java
@@ -0,0 +1,15 @@
+package net.salesianos.exercise.farmer;
+
+public class Farmer  extends Thread{
+    private String name;
+
+
+    public Farmer(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void run(){
+        // implement thread
+    }
+}
