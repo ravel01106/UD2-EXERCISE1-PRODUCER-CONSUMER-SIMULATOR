01693229875441c470e79f4fb291956de1d7d983
ravel01106 <ravello2002@gmail.com>
2023-11-09 21:57:31 +0000

    feat: create Vegetable class

--- src/net/salesianos/exercise/App.java
+++ src/net/salesianos/exercise/App.java
@@ -1,6 +1,5 @@
 package net.salesianos.exercise;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
     }
 }
---
+++ src/net/salesianos/exercise/vegetable/Vegetable.java
@@ -0,0 +1,48 @@
+package net.salesianos.exercise.vegetable;
+
+public class Vegetable {
+    final private static String[] VEGETABLE_NAMES = {
+        "lettuce",
+        "cabbage",
+        "onion",
+        "spinach",
+        "potato",
+        "celery",
+        "asparagus",
+        "radish",
+        "broccoli",
+        "artichoke",
+        "tomato",
+        "cucumber",
+        "eggplant",
+        "carrot",
+        "green bean"
+    };
+
+    private String name;
+    private int growingTime;
+
+    public Vegetable(){
+        this.name = selectName();
+        this.growingTime = calculateGrowingTime();
+    }
+
+
+    private String selectName(){
+        int indexRandom = (int)(Math.floor(Math.random()*(VEGETABLE_NAMES.length-0+1)+0));
+        return VEGETABLE_NAMES[indexRandom];
+    }
+
+    private int calculateGrowingTime(){
+        return (int)(Math.floor(Math.random()*(10-1+1)+1));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrowingTime() {
+        return growingTime;
+    }
+
+}
