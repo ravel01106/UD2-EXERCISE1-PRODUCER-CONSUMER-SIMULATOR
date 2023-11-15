4a5b45a21cd7db89dac82c6bbce271ae79e978d9
ravel01106 <amandaravelocabrera@gmail.com>
2023-11-15 16:32:33 +0000

    feat: the experiment three is done

--- src/net/salesianos/exercise/App.java
+++ src/net/salesianos/exercise/App.java
@@ -1,7 +1,5 @@
 package net.salesianos.exercise;
 
-import java.util.ArrayList;
-
 import net.salesianos.exercise.objects.restaurant.Restaurant;
 import net.salesianos.exercise.threads.customer.Customer;
 import net.salesianos.exercise.threads.farmer.Farmer;
@@ -10,42 +8,26 @@ import net.salesianos.exercise.utils.Utils;
 public class App {
 
     public static void main(String[] args){
-        Restaurant restaurant = new Restaurant(6);
-        ArrayList<Farmer> farmerCollection = new ArrayList<>();
-        ArrayList<Customer> customerCollection = new ArrayList<>();
+        Restaurant restaurant = new Restaurant(3);
 
-        Farmer farmerOne = new Farmer("Adrian", 10, restaurant);
-        Farmer farmerTwo = new Farmer("Saul", 10, restaurant);
-        farmerCollection.add(farmerOne);
-        farmerCollection.add(farmerTwo);
+        Farmer farmerOne = new Farmer("Saúl", 5, restaurant);
 
-        Customer customerOne = new Customer("Oliver", 5, restaurant);
-        Customer customerTwo = new Customer("David", 10, restaurant);
-        Customer customerThree = new Customer("Marco", 5, restaurant);
-        customerCollection.add(customerOne);
-        customerCollection.add(customerTwo);
-        customerCollection.add(customerThree);
+        Customer customerOne = new Customer("Marco", 5, restaurant);
 
         System.out.println("\t-> Initial warehouse status: " + restaurant.getVegetableCollectionSize());
+        farmerOne.start();
+        farmerOne.setPriority(Thread.MAX_PRIORITY);
+        customerOne.start();
+        
 
-        for (Farmer farmer : farmerCollection) {
-            farmer.start();
-        }
-        for (Customer customer : customerCollection) {
-            customer.start();
-        }
         try {
-            for (Farmer farmer : farmerCollection) {
-                farmer.join();
-            }
-            for (Customer customer : customerCollection) {
-                customer.join();
-            }
+            farmerOne.join();
+            customerOne.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        Utils.printResultThreads(farmerCollection, customerCollection, restaurant);
+        Utils.printResultThreads(farmerOne, customerOne, restaurant);
         
     }
 }
